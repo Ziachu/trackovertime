7303667a957c9d1f13aa2fd5e9d739e63bfb225f
Michał Ziach <ziachmichal@gmail.com>
2018-07-18 19:06:22 +0200

    Boilerplate

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<?php
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Track Over Time</title>
+
+    <link rel="stylesheet" type="text/css" href="./css/base.css" />
+    <link rel="stylesheet" type="text/css" href="./css/calendar.css" />
+    <link rel="stylesheet" type="text/css" href="./css/popup.css" />
+  </head>
+  <body>
+
+    <?php // TODO: show calendar (empty if first visit) ?>
+
+    <?php // TODO: show pop-up "Dodaj wyzwanie" if first visit ever ?>
+    
+    <?php // TODO: show pop-up "Wyzwanie opanowane?" if first visit after deadline ?>
+
+    <script src="./js/storage.js"></script>
+    <script src="./js/calendar.js"></script>
+  </body>
+</html>
